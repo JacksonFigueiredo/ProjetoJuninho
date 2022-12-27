fddf2d6eac8cfd5b5b0a0e1e65c40726028b60af
jackson.figueiredo <jackson.figueiredo@valid.com>
2022-12-27 13:37:45 -0300

    Incluindo Projeto de Uso de Properties C#

---
+++ Properties/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+   
+    
+    public class Produto
+    {
+        private int _quantidade { get; set; }
+        private string? _nome { get; set; }
+        private double _preco { get; }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set {  _nome = value; }
+        }
+
+        public string SetNome(string nome)
+        {
+            return _nome = nome;
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            produto.Nome = "Ademar";
+            Console.WriteLine(produto.Nome);
+        }
+    }
+}
