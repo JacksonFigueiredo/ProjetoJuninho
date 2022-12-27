8eaab66d32a11ddc3d949bb99a5eff05a546f996
jackson.figueiredo <jackson.figueiredo@valid.com>
2022-12-27 13:29:18 -0300

    Inclusão do Tutorial de Encapsulamento

---
+++ Encapsulamento/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+   
+    
+    public class Produto
+    {
+        private int _quantidade { get; set; }
+        private string? _nome { get; set; }
+        private double _preco { get; }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public string SetNome(string nome)
+        {
+            return _nome = nome;
+        }
+    }
+}
---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Produto produto = new Produto();
+
+            produto.SetNome("Jackson");
+            Console.WriteLine(produto.GetNome());
+        }
+    }
+}
