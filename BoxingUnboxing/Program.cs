e7a8cbee3de2309ca1d59ace175af802c21da085
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-18 18:33:58 -0300

    Boxing Unboxing

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+
+            Object obj = x; // Boxing - Jogando para o Heap
+
+            Console.WriteLine(obj);
+
+            int y = (int)obj; // Trazebdi do Heap.
+
+            Console.WriteLine(y);
+
+
+        }
+    }
+}
