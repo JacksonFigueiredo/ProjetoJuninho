9da8b96ee0d0e8bed53640a9e674f9f8fbde83aa
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-18 18:31:29 -0300

    Adicionando Projeto de Arrays.

---
+++ Vetores/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Vetores
+{
+    internal class Program
+    {
+        /* Vetores são nomes dados a arranjos unidimensionais. - Array
+         * Arranjo é uma estrutura de dados, sempre dados do mesmo tipo.
+         * Fácil para acessar os dados, armazenado de uma só vez em im bloco contiguo de memória, ordenada sendo acessados por posição.
+         * Desvantagem seria o tamanho fixo, e dificuldade para se realizar inserções e deleções.     
+         * */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
