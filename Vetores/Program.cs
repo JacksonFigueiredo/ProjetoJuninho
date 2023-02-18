5bf100b8552c1df7b6213ebe69c1b1042413c161
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-18 19:24:48 -0300

    Refatorando

--- Vetores/Program.cs
+++ Vetores/Program.cs
@@ -2,38 +2,46 @@
 {
     internal class Program
     {
-        /* Vetores são nomes dados a arranjos unidimensionais. - Array
-         * Arranjo é uma estrutura de dados, sempre dados do mesmo tipo.
-         * Fácil para acessar os dados, armazenado de uma só vez em im bloco contiguo de memória, ordenada sendo acessados por posição.
-         * Desvantagem seria o tamanho fixo, e dificuldade para se realizar inserções e deleções.     
-         * */
 
         static void Main(string[] args)
         {
-            int tamanhoVetor = 1000000000;
+            // Vetor de 1 Milhão de Posições.
+            int tamanhoVetor = 1000000;
             Random numeroAleatorio = new Random();
             double[] vetor = new double[tamanhoVetor];
 
             for (int i = 0; i < tamanhoVetor; i++)
             {
-                vetor[i] = numeroAleatorio.Next(1,50000000);
+                // Gerar Aleatório de 1 a 10 Milhões.
+                vetor[i] = numeroAleatorio.Next(1, 10000000);
             }
 
-            Console.WriteLine(String.Format("Soma : {0}",vetor.Sum()));
+            //Soma total da operação.
+            Console.WriteLine(String.Format("Soma : {0} \n", vetor.Sum()));
 
 
             var watch1 = System.Diagnostics.Stopwatch.StartNew();
-            Console.WriteLine(String.Format("A Média de Valores é : {0} ", vetor.Sum() / tamanhoVetor));
+            Console.WriteLine(String.Format("A Média de Valores é : {0}", vetor.Sum() / tamanhoVetor));
             watch1.Stop();
-            var elapsedMs1 = watch1.ElapsedMilliseconds;
-            Console.WriteLine("Elapsed 1 : " + elapsedMs1 + " Milisegundos");
+            var elapsedMs1 = watch1.ElapsedTicks;
+            Console.WriteLine("Corrido 1 : " + elapsedMs1 + " Ticks - Método Soma / Tamanho \n");
 
             var watch2 = System.Diagnostics.Stopwatch.StartNew();
-            Console.WriteLine(String.Format("A Média de Valores é : {0} Milisegundos", vetor.Average()));
+            Console.WriteLine(String.Format("A Média de Valores é : {0}", vetor.Average()));
             watch2.Stop();
-            var elapsedMs2 = watch2.ElapsedMilliseconds;
-            Console.WriteLine("Elapsed 2 : " + elapsedMs2 + " Milisegundos");
+            var elapsedMs2 = watch2.ElapsedTicks;
+            Console.WriteLine("Corrido 2 : " + elapsedMs2 + " Ticks - Método Média do Framework \n");
 
+            var watch3 = System.Diagnostics.Stopwatch.StartNew();
+            double soma = 0;
+            foreach (double v in vetor)
+            {
+                soma += v;
+            }
+            Console.WriteLine(String.Format("A Média de Valores é : {0}", soma / tamanhoVetor));
+            watch3.Stop();
+            var elapsedMs3 = watch3.ElapsedTicks;
+            Console.WriteLine("Corrido 3 : " + elapsedMs3 + " Ticks - Método Foreach");
 
         }
     }
