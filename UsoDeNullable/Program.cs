0b722380271f1cfae345a740c4e840ca395e5400
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-18 18:21:24 -0300

    Adicionando Uso de Nullables.

---
+++ UsoDeNullable/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UsoDeNullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> n = null; //Generic Type
+            int? o = null; // Value Type
+
+
+            Console.WriteLine(n.GetValueOrDefault()); // Se Nulo Sempre Retorne 0
+
+            Console.WriteLine(o.GetValueOrDefault()); // Se Nulo Sempre Retorne 0
+
+            if (o.HasValue && n.HasValue)
+            {
+
+            }
+
+        }
+    }
+}
