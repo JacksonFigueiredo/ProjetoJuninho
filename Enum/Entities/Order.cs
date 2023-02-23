22adc03ca2a00369ee39454f88f2adf2d6373e91
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-23 11:28:29 -0300

    Adicionando Enumeração.

---
+++ Enum/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatys Status { get; set; }
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Enum
+{
+    internal class Program
+    {
+        //Enum é representado pelo Tipo / Valor
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
