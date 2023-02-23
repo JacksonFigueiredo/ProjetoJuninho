0a0442fbcc1e00c7ba9c517e0f3ddd505875de6f
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-23 09:02:11 -0300

    Incluindo Switch Case

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool teste = true;
+
+            switch (teste)
+            {
+                case true:
+                    Console.WriteLine("Teste True");
+                    Console.WriteLine("Ok");
+                    break;
+                case false:
+                    Console.WriteLine("Teste Faulse");
+                    Console.WriteLine("Ok");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Nada");
+                    break;
+            }
+        }
+    }
+}
