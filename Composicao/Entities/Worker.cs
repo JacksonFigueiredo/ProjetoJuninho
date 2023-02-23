b1f2d0c639cc620144c4ca3f8cd9b4980c51f46f
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-23 12:40:14 -0300

    Trabalhando com Composicao.

---
+++ Composicao/Entities/Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicao.Entities
+{
+    class Worker
+    {
+        public string? Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+
+
+    }
+}
