662c57ac6e992a20e9bb2cb7a85d8befe73d3280
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-23 11:40:14 -0300

    Incluindo Enums

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EnumProject.Entities;
+using EnumProject.Entities.Enums;
 
 namespace EnumProject
 {
@@ -8,7 +9,21 @@ namespace EnumProject
 
         static void Main(string[] args)
         {
-            Order order = new Order();
+            Order order = new Order { 
+                Id = 1, Moment = DateTime.Now, Status = OrderStatus.PendingPayment
+            };
+
+            Order order2 = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = Enum.Parse<OrderStatus>("PendingPayment") // ou o numero
+            };
+
+            Console.WriteLine("{0}, {1}, {2}",order.Id,order.Moment,order.Status);
+            Console.WriteLine("{0}, {1}, {2}", order2.Id, order2.Moment, order2.Status);
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
 
         }
     }
