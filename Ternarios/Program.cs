966e1b5f23dade622a1b52c6e2aa3401ce8d401e
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-23 09:07:54 -0300

    Incluindo Ternários.

---
+++ Ternarios/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ternarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int preco = 10000;
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(desconto);
+        }
+    }
+}
