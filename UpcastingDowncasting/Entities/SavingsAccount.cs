d2f26da7d65d787c9786b9f2214acc0107fc2dd5
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-27 11:55:16 -0300

    Um Pouco de Downcasting e UpCasting

---
+++ UpcastingDowncasting/Entities/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingDowncasting.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance) // irá trazer de base para o ctor do savingsacount
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+    }
+}
---
+++ UpcastingDowncasting/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using UpcastingDowncasting.Entities;
+
+namespace UpcastingDowncasting
+{
+    internal class Program
+    {
+
+        //Upcasting
+        //Casting da subclasse para supeclasse.
+        //Uso Comum : Polimorfismo
+
+
+        //Downcasting
+
+        //Casting da superclasse para subclasse
+        //palavra as
+        //palavra is
+        //uso comun : metodos que recebem parametros genericos.
+
+
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1, "Bob", 0.0, 200);
+            Account acc3 = new SavingsAccount(2, "Ana", 0.0, 0.01);
+
+
+            //DOWNCASTING.
+
+            BusinessAccount accountacc4 = (BusinessAccount)acc2;
+
+            BusinessAccount accountacc5 = (BusinessAccount)acc3;
+
+
+        }
+    }
+}
