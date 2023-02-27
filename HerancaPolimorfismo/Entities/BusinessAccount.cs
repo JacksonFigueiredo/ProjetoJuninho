b5adb6865872890de0de444360ec1889c87e09f5
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-02-27 10:35:33 -0300

    Adicionando Herança e Polimorfismo

---
+++ HerancaPolimorfismo/Entities/BusinessAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaPolimorfismo.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance) // pegando o construtor da classe base
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount < LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ HerancaPolimorfismo/Program.cs
@@ -0,0 +1,17 @@
+﻿using HerancaPolimorfismo.Entities;
+
+namespace HerancaPolimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           BusinessAccount businessAccount = new BusinessAccount(1,"Jackson",1000,5000);
+
+            businessAccount.Deposit(1000);
+            businessAccount.Loan(1000);
+
+            Console.WriteLine(businessAccount.Balance);
+        }
+    }
+}
