617c93f83f08d161bf657fe7e6722367e19d176c
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-03 16:18:12 -0300

    Adicionando Extension Methods.

---
+++ ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime dateTime)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(dateTime);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return String.Format("O Tempo Total é {0} Horas", duration);
+            }
+            else
+            {
+                return String.Format("O Tempo Total é {0} Dias", duration);
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DateTime.Now.Subtract(TimeSpan.FromHours(1)).ElapsedTime());
+        }
+    }
+}
