4bd0d3f6ed32c93ae33a876463b11c01003ef2ba
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-03 13:14:50 -0300

    Incluindo HashSet

---
+++ HashSet/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSet
+{
+    class Pessoa
+    {
+        public string NomePessoa { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nomePessoa, int idade)
+        {
+            NomePessoa = nomePessoa;
+            Idade = idade;
+        }
+
+        public Pessoa()
+        {
+
+        }
+    }
+}
---
+++ HashSet/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Pessoa> hashset = new HashSet<Pessoa>();
+
+            hashset.Add(new Pessoa { NomePessoa = "Jackson", Idade = 32 });
+            hashset.Add(new Pessoa { NomePessoa = "Kaylla", Idade = 5 });
+            hashset.Add(new Pessoa { NomePessoa = "Ketlen", Idade = 31 });
+
+
+            int soma = 0;
+            foreach (var p in hashset)
+            {
+                soma += p.Idade;
+            }
+
+            Console.WriteLine("A Idade de todos é {0}",soma);
+
+        }
+    }
+}
