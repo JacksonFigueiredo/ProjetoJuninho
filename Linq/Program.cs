11ea9b88e08130d0a1cd4a9ed7d4fa7b86911154
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-05 17:33:57 -0300

    Adicionando Uso de Linq

---
+++ Linq/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Linq
+{
+    public class Program
+    {
+
+    
+
+        static void Main(string[] args)
+        {                       
+            int[] pairNumbers = { 1, 2, 3, 4, 5, };
+            List<int> evenNumbers = pairNumbers.Where(n => n % 2 == 0).ToList();
+          
+            foreach (int n in evenNumbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+    }
+}
